7d3328609b271a3a8b9fa67e47528885a3cacab2
nathanieljramirez <nathaniel_ramirez@icloud.com>
2026-06-06 23:43:24 -0700

    Update main.go

--- src/main.go
+++ src/main.go
@@ -14,31 +14,26 @@ func main() {
 	command := os.Args[1]
 	arguments := os.Args[2:]
 	flags := map[string]string{
-		"name":    "",
-		"version": "latest",
+		"--name":    "",
+		"--version": "latest",
 	}
 
 	for i, arg := range arguments {
-		if arg == "--name" {
-			fmt.Println("Creating server...")
+		if _, ok := flags[arg]; ok {
 			if !strings.HasPrefix(arguments[i+1], "--") {
-				flags["name"] = arguments[i+1]
-			}
-		} else if arg == "--version" {
-			if !strings.HasPrefix(arguments[i+1], "--") {
-				flags["version"] = arguments[i+1]
+				flags[arg] = arguments[i+1]
 			}
 		}
 	}
 
 	switch command {
 	case "create":
-		if flags["name"] == "" {
+		if flags["--name"] == "" {
 			fmt.Println("--name needed to create server")
 			os.Exit(1)
 		}
 
-		os.Mkdir(flags["name"], 0755)
+		os.Mkdir(flags["--name"], 0755)
 	case "start":
 		fmt.Println(command, arguments)
 		os.Exit(0)
