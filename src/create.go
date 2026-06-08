45a150057d4093619f50771e3cfdb85f277df27c
nathanieljramirez <nathaniel_ramirez@icloud.com>
2026-06-08 13:23:31 -0700

    Set server creation's working directory to world name

--- src/create.go
+++ src/create.go
@@ -8,23 +8,21 @@ import (
 
 func create(world string) {
 	fmt.Println("Creating server...")
+	os.Mkdir(world, 0755)
 	config := readConfig()
 
-	err := os.WriteFile("eula.txt", []byte("eula=true"), 0644)
+	err := os.WriteFile(world+"/eula.txt", []byte("eula=true"), 0644)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 
-	fmt.Println("create server.")
-
 	cmd := exec.Command("java", "-jar", config.Jar)
+	cmd.Dir = world
 	err = cmd.Run()
 
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-
-	os.Mkdir(world, 0755)
 }
