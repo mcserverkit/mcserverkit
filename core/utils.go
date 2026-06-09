7b9146a9b06fa264056fe9ad61601729db0867c2
nathanieljramirez <nathaniel_ramirez@icloud.com>
2026-06-08 21:13:38 -0700

    Move DownloadFile and Parse functions to separate utils.go file

---
+++ core/utils.go
@@ -0,0 +1,57 @@
+package core
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func Parse(url string, target any) {
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Error requesting URL:", err)
+		os.Exit(1)
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		fmt.Println(resp.Status)
+		os.Exit(1)
+	}
+
+	err = json.NewDecoder(resp.Body).Decode(target)
+
+	if err != nil {
+		fmt.Println("Error decoding JSON:", err)
+		os.Exit(1)
+	}
+}
+
+// https://stackoverflow.com/questions/11692860/how-can-i-efficiently-download-a-large-file-using-go
+func DownloadFile(filepath string, url string) error {
+	out, err := os.Create(filepath)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		return fmt.Errorf("%s", resp.Status)
+	}
+
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
