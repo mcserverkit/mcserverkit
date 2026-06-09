417d0a31b1fd19d686ab4838332935e20babcadf
nathanieljramirez <nathaniel_ramirez@icloud.com>
2026-06-08 21:28:23 -0700

    Move Create function to commands.go

--- core/commands.go
+++ core/commands.go
@@ -1,1 +1,22 @@
 package core
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+func Create(name string) error {
+	fmt.Println("Creating server...")
+	err := os.Mkdir(name, 0755)
+	if err != nil {
+		return err
+	}
+
+	err = os.WriteFile(filepath.Join(name, "eula.txt"), []byte("eula=true"), 0644)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
