0eebe0add5bf814669c316a5818f6c772cbed3ee
nathanieljramirez <nathaniel_ramirez@icloud.com>
2026-06-27 15:42:53 -0700

    Move reference note closer to operating system check logic

--- bindings/rust/build.rs
+++ bindings/rust/build.rs
@@ -1,7 +1,7 @@
-// https://stackoverflow.com/questions/43292357/how-can-one-detect-the-os-type-using-rust
 fn main() {
     println!("Hello World!");
 
+    // https://stackoverflow.com/questions/43292357/how-can-one-detect-the-os-type-using-rust
     if cfg!(target_os = "windows") {
     } else if cfg!(target_os = "linux") {
     } else if cfg!(target_os = "macos") {
