15a54054d34ee187712737381383166a8e2372f7
nathanieljramirez <nathaniel_ramirez@icloud.com>
2026-06-29 12:03:39 -0700

    Check if operating system is supported instead of checking individually

--- bindings/rust/build.rs
+++ bindings/rust/build.rs
@@ -4,9 +4,8 @@ fn main() {
     println!("Hello World!");
 
     // https://stackoverflow.com/questions/43292357/how-can-one-detect-the-os-type-using-rust
-    let _output = if cfg!(target_os = "windows") {
-    } else if cfg!(target_os = "linux") {
-    } else if cfg!(target_os = "macos") {
+    let _output = if cfg!(any(target_os = "windows", target_os = "linux", target_os = "macos"))
+    {
     } else {
         println!("Operating system not supported");
         return;
