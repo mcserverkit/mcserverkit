8e327146ed8681fdf6a8962388334e02e2158fad
nathanieljramirez <nathaniel_ramirez@icloud.com>
2026-06-29 13:27:20 -0700

    Remove test hello world

--- bindings/rust/build.rs
+++ bindings/rust/build.rs
@@ -1,6 +1,4 @@
 fn main() {
-    println!("Hello World!");
-
     // https://stackoverflow.com/questions/43292357/how-can-one-detect-the-os-type-using-rust
     let _filename = if cfg!(target_os = "windows") {
         "mcserverkit.windows-x86_64.zip";
