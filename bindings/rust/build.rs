ad1b051c2a3993054767c5cb8e993cac456f4725
nathanieljramirez <nathaniel_ramirez@icloud.com>
2026-06-29 12:05:05 -0700

    Format and remove command process

--- bindings/rust/build.rs
+++ bindings/rust/build.rs
@@ -1,11 +1,12 @@
-use std::process::Command;
-
 fn main() {
     println!("Hello World!");
 
     // https://stackoverflow.com/questions/43292357/how-can-one-detect-the-os-type-using-rust
-    let _output = if cfg!(any(target_os = "windows", target_os = "linux", target_os = "macos"))
-    {
+    let _output = if cfg!(any(
+        target_os = "windows",
+        target_os = "linux",
+        target_os = "macos"
+    )) {
     } else {
         println!("Operating system not supported");
         return;
