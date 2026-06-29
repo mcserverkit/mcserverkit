90cbcab1fb74cb70c0955d4ce1bbfc7f8246fee9
nathanieljramirez <nathaniel_ramirez@icloud.com>
2026-06-29 15:25:48 -0700

    Remove semicolons to return filename value properly

--- bindings/rust/build.rs
+++ bindings/rust/build.rs
@@ -1,13 +1,15 @@
 fn main() {
     // https://stackoverflow.com/questions/43292357/how-can-one-detect-the-os-type-using-rust
-    let _filename = if cfg!(target_os = "windows") {
-        "mcserverkit.windows-x86_64.zip";
+    let filename = if cfg!(target_os = "windows") {
+        "mcserverkit.windows-x86_64.zip"
     } else if cfg!(target_os = "linux") {
-        "mcserverkit.linux-x86_64.zip";
+        "mcserverkit.linux-x86_64.zip"
     } else if cfg!(target_os = "macos") {
-        "mcserverkit.macos-x86_64.zip";
+        "mcserverkit.macos-x86_64.zip"
     } else {
         println!("Operating system not supported");
         return;
     };
+
+    println!("{filename}");
 }
